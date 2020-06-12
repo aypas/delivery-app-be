49a128ab17985cfe57ac4a78f42a34bcdf3bd3b5
aypas <tecimler96@gmail.com>
2020-06-12 16:19:10 -0400

    mistake so tiny

--- authentication/misc.py
+++ authentication/misc.py
@@ -5,5 +5,5 @@ import google_auth_oauthlib.flow
 flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
 		    		os.path.join(os.path.dirname(__file__), 'client_secret.json'),
 		    		scopes=["https://www.googleapis.com/auth/gmail.readonly"],
-		    		redirect_uri = "https://a-delivery.tk/accounts/oauth2callback"
+		    		redirect_uri = "https://api.a-delivery.tk/api/auth/oauth2callback/"
 		    )
